5ebf02d6cacf2cf914ae16c6dd4b7ed82d254aa1
Md.Redwanul haque <32310502+Redwan-anik@users.noreply.github.com>
2019-11-14 18:36:14 +0600

    Add files via upload

---
+++ Newtons_law.py
@@ -0,0 +1,38 @@
+from pylab import plot,show,legend,title,xlabel,ylabel,savefig,legend
+
+
+
+def create_graph(x,y):
+    plot(x,y,marker='o')
+    xlabel('Distance in meter ')
+    ylabel('grivitional Force')
+    title('gravitional force ')
+    legend([100, 1001, 50])
+    
+    
+    
+def generate_F_r():
+    r = range(100,1001,50)
+    F=[]
+    G= 6.674*(10**-11)
+    m1=0.5
+    m2=1.5
+    savefig('graph.png')
+    
+    for dist in r:
+        force=G*(m1*m2)/(dist**2)
+        
+        F.append(force)
+        
+    create_graph(r,F)
+
+if __name__ == '__main__':
+    generate_F_r()
+    
+    
+
+        
+    
+        
+        
+    
